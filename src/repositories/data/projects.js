cc3bea1f3bdccda2abd6d508e8191dc24426b42f
Jonathan Dequeker <jonathan.dequeker@gmail.com>
2018-12-30 12:03:37 +0100

    project description markup

--- src/repositories/data/projects.js
+++ src/repositories/data/projects.js
@@ -36,10 +36,8 @@ export default {
       },
       {
         description: `
-          <h1>Test</h1>
-          <p>
-            Kolom is de ruggengraat die de boeken een vertikale positie in de ruimte laat aannemen.
-          </p>
+          <p><i>Kolom</i> is een vrijstaande boekenkast.<br>Ze bestaat uit een moduleerbare houten structuur die op een betonnen sokkel pivoteert. In situ brengt het meubel een bepalende architecturale kwaliteit (visuele referentie): de circulaire vorm doet de aanschouwer denken aan een zuil terwijl de horizontale onderverdeling plaats biedt voor het rangschikken van boeken.</p>
+          <p>Vervaardigd uit Afromosiahout vormt deze opeenvolging van x-en een ritmische en opwaartse geometrie die de mogelijkheid van een oneindige uitzetting suggereert. Ze rust op een betonnen voetstuk die als pivot en verbinding dient tussen de grond en de houten structuur. Het geheel vormt een microarchitectuur die functie en vorm verheffen tot een nieuwe harmonie.</p>
         `
       }
     ]
@@ -84,7 +82,12 @@ export default {
         description: ''
       },
       {
-        description: 'Voor het inrichten van een koffiehuis in een voormalige Gentse burgerwoning, was het noodzakelijk om bepaalde elementen uit het interieur te halen en ontbrekende voorzieningen toe te voegen. We hebben deze twee handelingen echt als  afzonderlijke interventies beschouwd. Ze afzonderlijk ontwerpen verduidelijkte meteen aan welke elementen er waarde wordt gehecht. Het weghalen stond voor ons steeds in functie van het beter laten uitkomen van bestaande delen maar het toont zich ook fysiek in de ruimte, met een massa en vaak ook kleuraanduiding. Enkel waar het oorspronkelijke interieur voorzieningen miste werden elementen toegevoegd, die dan wel afstand houden van hun omgeving zodat ze een open interactie aangaan met de gebruiker. Het zijn vrijblijvende, haast abstracte volumes die hun gebruik subtiel suggereren. In geen geval zouden ze het oorspronkelijke interieur willen imiteren of beïnvloeden. We hopen dat door de manier waarop we weggenomen en toegevoegd hebben, er een omgeving is ontstaan die de uniciteit in ere houdt en uitnodigt om ze ongedwongen te gaan ontdekken.'
+        description: `
+          <p>Voor het inrichten van een koffiehuis in een voormalige burgerwoning, was het noodzakelijk om bepaalde elementen uit het interieur te halen en ontbrekende voorzieningen toe te voegen.</p>
+          <p>We hebben deze twee handelingen echt als afzonderlijke interventies beschouwd. Ze afzonderlijk ontwerpen verduidelijkte meteen aan welke elementen er waarde wordt gehecht. Het weghalen stond voor ons steeds in functie van het beter laten uitkomen van bestaande delen maar het toont zich ook fysiek in de ruimte, met een massa en vaak ook kleuraanduiding.</p>
+          <p>Enkel waar het oorspronkelijke interieur voorzieningen miste werden elementen toegevoegd, die dan wel afstand houden van hun omgeving zodat ze een open interactie aangaan met de gebruiker. Het zijn vrijblijvende, haast abstracte volumes die hun gebruik subtiel suggereren. In geen geval zouden ze het oorspronkelijke interieur willen imiteren of beïnvloeden.</p>
+          <p>We hopen dat door de manier waarop we weggenomen en toegevoegd hebben, er een omgeving is ontstaan die de uniciteit in ere houdt en uitnodigt om ze ongedwongen te gaan ontdekken.</p>
+        `
       }
     ]
   },
@@ -108,7 +111,11 @@ export default {
         description: ''
       },
       {
-        description: 'Still is altijd in beweging voor de aanschouwer. De structuur van rond staal lijkt toch telkens weer te ontsnappen aan zijn begrip ervan. Door rond het object te stappen verandert het beeld, terwijl de onderliggende structuur als een schim zichtbaar wordt. Zo complex de structuur, zo eenvoudig het tafelblad; een massief stuk hardhout met nét de juiste afmetingen op nét de juiste hoogte.'
+        description: `
+          <p>Still is altijd in beweging voor de aanschouwer.</p>
+          <p>De structuur van rond staal lijkt toch telkens weer te ontsnappen aan zijn begrip ervan. Door rond het object te stappen verandert het beeld, terwijl de onderliggende structuur als een schim zichtbaar wordt.</p>
+          <p>Zo complex de structuur, zo eenvoudig het tafelblad; een massief stuk hardhout met nét de juiste afmetingen op nét de juiste hoogte.</p>
+        `
       }
     ]
   },
@@ -152,7 +159,19 @@ export default {
         description: ''
       },
       {
-        description: 'Uitgestrekte velden over het glooiende heuvelland brachten een scherpe wind uit het noordwesten. Een poreuze structuur vangt ze op en wendt ze af: enkel de hoogste takken getuigen nog van de harde wind. Herfst, winter, lente, zomer: de akkers dragen een kleurrijke garderobe. Glas, als stilstaand water, vertroebelt het landschapskleed tot vlakken van kleur. Een filter om bij het opkijken in de velden veel meer te zien. Een crescendo voor het oog, valt de blik van links naar rechts de verte in naar de stad en haar hoogste torens. Een barrière van hardhouten latten golft speels met het landschap mee, eerst hoog, dan geleidelijk lager. Om de kijker het deinzen van de ogen aan te leren en ze daarna te belonen. Oude stenen werpen onbekommerd schaduw over verleden en toekomst. Water weerkaatst nu een heldere hemel. Het is een oninneembare kalmte die de dorre dromen klaart. Een ruimte los en overgaand, tussen weidse wereld en veilig binnen. Een verdiepingshoge structuur biedt steun aan het wolkendak, omlijst muren van tuin, met vensters tussen wilgenbomen. Een gloriette: om geborgen buiten te vertoeven, een anker voor het huis, een uitnodiging om tussen natuur te tafelen.'
+        description: `
+          <p>Uitgestrekte velden over het glooiende heuvelland brachten een scherpe wind uit het noordwesten.<br>
+          Een poreuze structuur vangt ze op en wendt ze af: enkel de hoogste takken getuigen nog van de harde wind.</p>
+          <p>Herfst, winter, lente, zomer: de akkers dragen een kleurrijke garderobe.<br>
+          Glas, als stilstaand water, vertroebelt het landschapskleed tot vlakken van kleur. Een filter om bij het opkijken in de velden veel meer te zien.</p>
+          <p>Een crescendo voor het oog, valt de blik van links naar rechts de verte in naar de stad en haar hoogste torens.<br>
+          Een barrière van hardhouten latten golft speels met het landschap mee, eerst hoog, dan geleidelijk lager. Om de kijker het deinzen van de ogen aan te leren en ze daarna te belonen.</p>
+          <p>Oude stenen werpen onbekommerd schaduw over verleden en toekomst.<br>
+          Water weerkaatst nu een heldere hemel. Het is een oninneembare kalmte die de dorre dromen klaart.<br>
+          Een ruimte los en overgaand, tussen weidse wereld en veilig binnen.</p>
+          <p>Een verdiepingshoge structuur biedt steun aan het wolkendak, omlijst muren van tuin, met vensters tussen wilgenbomen.<br>
+          Een <i>gloriëtte:</i> om geborgen buiten te vertoeven, een anker voor het huis, een uitnodiging om tussen natuur te tafelen.</p>
+        `
       }
     ]
   }
