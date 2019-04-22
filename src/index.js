01f3fccbe07e3068834a857eb9b274a3ab4708e3
Niels Dequeker <niels@newfront.eu>
2019-04-22 17:30:49 +0200

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-snapshot';
-import './index.css';
-import App from './containers/App/App.js';
-import registerServiceWorker from './registerServiceWorker';
-
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
