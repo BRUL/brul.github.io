aaf805cf3ae5d552849850db44bd7b4a4a5648a8
BRUL <jonathan.dequeker@gmail.com>
2020-05-07 22:29:06 +0200

    Updates

---
+++ precache-manifest.be2d1aaba1f3bf25c4bed905e7e2b39c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac7b8be717ed42f7a8b1910dc6f159c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ffa0fb13ea83e8bf9b4",
+    "url": "/static/css/main.88557dea.chunk.css"
+  },
+  {
+    "revision": "c38e16dbbe6ec64e596c",
+    "url": "/static/js/2.aeee51da.chunk.js"
+  },
+  {
+    "revision": "eb33efc9d496f181d704b97efb253cc1",
+    "url": "/static/js/2.aeee51da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffa0fb13ea83e8bf9b4",
+    "url": "/static/js/main.732e5330.chunk.js"
+  },
+  {
+    "revision": "07173d8de64e27038c88",
+    "url": "/static/js/runtime-main.267012a8.js"
+  },
+  {
+    "revision": "8c1f1dc43b019f417aacb1a6182c1d67",
+    "url": "/static/media/341DA0_0_0.8c1f1dc4.woff"
+  },
+  {
+    "revision": "b0ef9706a4c116953d3574ee90011c3b",
+    "url": "/static/media/341DA0_0_0.b0ef9706.eot"
+  },
+  {
+    "revision": "b7ac06dfeab99350c87024c1477bd60d",
+    "url": "/static/media/341DA0_0_0.b7ac06df.ttf"
+  },
+  {
+    "revision": "ea643dfb70d762ae839f2516ca97be7b",
+    "url": "/static/media/341DA0_0_0.ea643dfb.woff2"
+  },
+  {
+    "revision": "0d6962d838101abc465f93b5a13256b8",
+    "url": "/static/media/NEJD-logo.0d6962d8.svg"
+  }
+]);
